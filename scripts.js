23a2861ede8d9b0d2bd8376d3aa853f9a55068f5
Szymon Halt <39597008+SEFMVA@users.noreply.github.com>
2020-06-10 01:31:21 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 console.error(AFRAME);
+document.addEventListener("camera-init", () => {
+    console.log("Camera init event");
+})
+
+document.addEventListener("markerFound", () => {
+    console.log("Marker found!");
+})
