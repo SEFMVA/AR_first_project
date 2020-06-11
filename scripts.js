6ad4233b0be7f7a2fac0da8b4416949f62b05760
Szymon Halt <39597008+SEFMVA@users.noreply.github.com>
2020-06-11 20:28:56 +0200

    test shrek

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-console.error(AFRAME);
-document.addEventListener("camera-init", () => {
-    console.log("Camera init event");
-})
-
 document.addEventListener("markerFound", () => {
     console.log("Marker found!");
 })
