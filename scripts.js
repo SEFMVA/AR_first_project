54598b1537f95c01fe572c3afdbfc3aa5aa06b6a
Szymon Halt <39597008+SEFMVA@users.noreply.github.com>
2020-06-22 01:22:46 +0200

    TODO

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+var DONUT = null
+var AUDIO = new Audio("eatSound.mp3")
+
 document.addEventListener("markerFound", () => {
     console.log("Marker found!");
+    if (DONUT === null) {
+        console.log("TODO create donut")
+
+        DONUT.addEventListener("click", () => {
+            console.log("TODO start animation")
+            AUDIO.play()
+        })
+
+    }
+})
+
+AUDIO.addEventListener("ended", () => {
+    console.log("TODO delete donut")
+    DONUT = null
 })
