c95ccdf3c56238c16a695d0cb691d93d27d934fc
Szymon Halt <39597008+SEFMVA@users.noreply.github.com>
2020-06-22 18:55:54 +0200

    test

--- scripts.js
+++ scripts.js
@@ -3,6 +3,15 @@ document.addEventListener("markerFound", () => {
 
 })
 
+AFRAME.registerComponent('markerhandler', {
+    init: function () {
+        this.el.sceneEl.addEventListener('markerFound', () => {
+            // redirect to custom URL
+            window.location = 'https://github.com/AR-js-org/AR.js';
+        });
+    }
+});
+
 // AFRAME.registerComponent('createDonut', {
 //     init: function () {
 //         const el = this.el;
